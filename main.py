8795f729bf8b87d8381cd1abda660c7bb9b7b59c
Exr0n <mail@exr0n.com>
2021-06-29 14:04:26 -0700

    static param matrices finalized?

--- main.py
+++ main.py
@@ -3,17 +3,39 @@
 
 config = {
     'size': {
-        'embedding_dim': 768,
-        'num_blocks': 12,
+        'emb_dim': 768,     # (attn_heads = 12) * (attn_dim = 64)
+        'stack_height': 12,
         'attn_heads': 12,
         'ctx_size': 1024,
+        'vocab_size': 50257,
         }
     }
 
-shapes = {
-        'embedding': {
-            'ctx': (config['size']['ctx_size'], config['size']['embedding_dim']),
-            }
+def get_shapes():
+    sz = config['size']
+    return {
+            'embedding': {
+                'positional': (sz['ctx_size'], sz['emb_dim']),
+                'token': (sz['vocab_size'], sz['emb_dim']),
+            },
+            'decoder': [{
+                'attn': {
+                    'query_w': (sz['emb_dim'], sz['emb_dim']),
+                    'query_b': (sz['emb_dim'], sz['emb_dim']),
+                    'key_w':   (sz['emb_dim'], sz['emb_dim']),
+                    'key_b':   (sz['emb_dim'], sz['emb_dim']),
+                    'value_w': (sz['emb_dim'], sz['emb_dim']),
+                    'value_b': (sz['emb_dim'], sz['emb_dim']),
+                    'proj_w':  (sz['emb_dim'], sz['emb_dim']),
+                    'proj_b':  (sz['emb_dim'], sz['emb_dim']),
+                },
+                'dense': {
+                    'fc_w':    (sz['emb_dim']*4, sz['emb_dim']),
+                    'fc_b':    (sz['emb_dim']*4, sz['emb_dim']),
+                    'proj_w':  (sz['emb_dim'], sz['emb_dim']*4),
+                    'proj_b':  (sz['emb_dim'], sz['emb_dim']*4),
+                }
+            }]*sz['stack_height']
         }
 
 import numpy as np
@@ -24,13 +46,11 @@ def initialize(shapes):
     it = shapes.items() if type(shapes) is dict else enumerate(shapes)
     for k, v in it:
         if type(v) is dict or type(v) is list:
-            v = initialize(v)
+            shapes[k] = initialize(v)
         elif type(v) is tuple:
             shapes[k] = rng.standard_normal(v)
-        else:
-            raise NotImplementedError('allocation description must be a dict of shape-tuples')
     return shapes
 
 if __name__ == '__main__':
-    mats = initialize(shapes)
+    mats = initialize(get_shapes())
     print(mats)
