c042d9e6601ef8dff09bb976c0de3b966dfaa95c
Exr0n <mail@exr0n.com>
2021-06-29 15:56:58 -0700

    why can't I slice the ndarray 2:4??

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ import numpy as np
 from numpy.random import default_rng
 
 config = {
+    # 'size': {
+    #     'emb_dim': 768,     # (attn_heads = 12) * (attn_dim = 64)
+    #     'stack_height': 12,
+    #     'attn_heads': 12,
+    #     'ctx_size': 1024,
+    #     'vocab_size': 50257,
+    #     }
     'size': {
-        'emb_dim': 768,     # (attn_heads = 12) * (attn_dim = 64)
-        'stack_height': 12,
-        'attn_heads': 12,
+        'emb_dim': 4,     # (attn_heads = 12) * (attn_dim = 64)
+        'stack_height': 2,
+        'attn_heads': 2,
         'ctx_size': 1024,
         'vocab_size': 50257,
         }
@@ -52,35 +59,59 @@ def initialize(shapes):
     return shapes
 
 
-def nonlin(x: np.ndarray):
+def nonlin(x: np.ndarray):  # NTFS: convert to in place operation
     # return x * (x > 0)    # relu
     from scipy.special import erf
     return x * 0.5 * (1 + erf(x/np.sqrt(2)))   # gelu
 
 
+def softmax(x: np.ndarray): # NTFS: convert to in place operation
+    ret = np.exp(x)
+    # print(ret)
+    sums = np.sum(ret, 1)
+
+    # print('softmax:', ret, sums)
+
 def feed_forward(dense, x):
     assert(x.shape == (config['size']['emb_dim'], seq_len))
+    # TODO: layernorm
     return nonlin(dense['proj_b'] + dense['proj_w'].dot(
-           nonlin(dense[  'fc_b'] + dense[  'fc_w'].dot(x))
-    ))
+           nonlin(dense[  'fc_b'] + dense[  'fc_w'].dot(x))     # NTFS: new memory initialized (emb_dim * 4, seq_len)
+    )) + x
 
 
 def self_attention(attn, x):
-    return x # TODO
+    assert(x.shape == (config['size']['emb_dim'], seq_len))
 
+    adim = config['size']['emb_dim'] // config['size']['attn_heads']
+
+    q, k, v = attn['query_w'].dot(x), attn['key_w'].dot(x), attn['value_w'].dot(x)  # NTFS: new memory initialized
+
+    print(q.tolist())
+
+    for hdi in range(config['size']['attn_heads']):     # NTFS: loop can be parallelized
+        q, k, v = q[hdi*adim:(hdi+1)*adim, :], k[hdi*adim:(hdi+1)*adim, :], v[hdi*adim:(hdi+1)*adim, :]
+        print(hdi*adim,(hdi+1)*adim, q[2:4]) # TODO: what the hell
+        print('q', q)
+        print('k', k)
+        print('v', v)
+        scalars = q.T.dot(k) / np.sqrt(adim)
+        scalars = softmax(scalars)
+
+    return x
 
 rng = default_rng(1336)
-seq_len = 15
+seq_len = 5
 
 if __name__ == '__main__':
     mats = initialize(get_shapes())
     # print(mats)
 
     inp = rng.standard_normal((config['size']['emb_dim'], seq_len))
-    print(inp)
+    # print(inp)
 
     for decoder in mats['decoder']:
         attn, dense = decoder.values()
         x = self_attention(attn, inp)
         inp = feed_forward(dense, x)
-        print(inp)
+        # print(inp)
