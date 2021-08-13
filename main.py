d4651cd8d4ee4a43eadcc937699806a63fcbb85a
mrMuffin157 <71684661+mrMuffin157@users.noreply.github.com>
2021-08-12 21:40:01 -0700

    Update main.py

--- main.py
+++ main.py
@@ -166,37 +166,6 @@ def infer(inp: np.ndarray):
         inp = feed_forward(dense, x)
     return inp
 
-
-# BEGIN YOINKS from andrew
-#   class PositionalEncoding(nn.Module):
-#     def __init__(self,
-#                  emb_size: int,
-#                  dropout: float,
-#                  maxlen: int = 5000):
-#         super(PositionalEncoding, self).__init__()
-#         pos = torch.arange(0, maxlen).reshape(maxlen, 1)
-#         den = torch.exp(- torch.arange(0, emb_size, 2)* math.log(10000) / emb_size)
-#         pos_embedding = torch.zeros((maxlen, emb_size))
-#         pos_embedding[:, 0::2] = torch.sin(pos * den)
-#         pos_embedding[:, 1::2] = torch.cos(pos * den)
-#         pos_embedding = pos_embedding.unsqueeze(-2)
-#
-#         self.dropout = nn.Dropout(dropout)
-#         self.register_buffer('pos_embedding', pos_embedding)
-#
-#     def forward(self, token_embedding: Tensor):
-#         return self.dropout(token_embedding + self.pos_embedding[:token_embedding.size(0), :])
-#
-# class TokenEmbedding(nn.Module):
-#     def __init__(self, vocab_size: int, emb_size):
-#         super(TokenEmbedding, self).__init__()
-#         self.embedding = nn.Embedding(vocab_size, emb_size)
-#         self.emb_size = emb_size
-#
-#     def forward(self, tokens: Tensor):
-#         return self.embedding(tokens.long()) * math.sqrt(self.emb_size)
-# END YOINKS
-
 rng = default_rng(1333)
 seq_len = 11
 
